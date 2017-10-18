2e8dcf122991af875b7d08db9b90154e146e914d
Meghajit Mazumdar <meghajit.254919@ad.infosys.com>
2017-10-18 15:37:54 +0530

    5th

--- src/components/LandingPageCard.js
+++ src/components/LandingPageCard.js
@@ -9,20 +9,18 @@ class LandingPageCard extends React.Component{
       render()
       {
           return(
-            
-            <div>
-               
-                      <div class="card">
-                        <div class="card-header">
-                        {this.props.headername}
-                        </div>
-                        <div class="card-block">
-                            <h3 class="card-title">{this.props.cardtitle}</h3>
-                            <p class="card-text">{this.props.cardtext}</p>
-                            <a href="#">  <button type="button" class="btn btn-primary btn-lg btn-block">{this.props.buttontext}</button></a>
-                        </div>
-                      </div>
-             </div>
+            <div class="col-xs-6">
+              <div class="card">
+                <div class="card-header">
+                  {this.props.headername}
+                </div>
+                <div class="card-block">
+                  <h3 class="card-title">{this.props.cardtitle}</h3>
+                  <p class="card-text">{this.props.cardtext}</p>
+                  <a href="#">  <button type="button" class="btn btn-primary btn-lg btn-block">{this.props.buttontext}</button></a>
+                </div>
+              </div>
+            </div>
           )}
         }
 
